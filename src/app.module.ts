f28746a86c63204fab12e59347d5727c4d383e20
Marcio Barroso <barrrosomarcio@gmail.com>
2025-01-14 10:28:55 -0300

    feat add lotofacilmodule to app and fix local execution

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LotoFacilModule } from './lotofacil/lotofacil.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LotoFacilModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -3,12 +3,13 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { Migrations } from './migrations/migrations';
 
 export const DatabaseProvider = new DataSource({
+  type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
   port: 5432,
+  database: process.env.POSTGRES_DATABASE || 'fezinha',
+  schema: 'public',
   username: process.env.POSTGRES_USER || 'fezinha',
   password: process.env.POSTGRES_PASSWORD || 'password',
-  database: process.env.POSTGRES_DATABASE || 'fezinha',
-  type: 'postgres',
   entities: [ConcursoLotoFacilEntity],
   synchronize: true,
   migrations: Migrations,
